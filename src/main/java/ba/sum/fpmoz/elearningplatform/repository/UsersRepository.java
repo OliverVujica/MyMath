08ca58b7b855c29f1186aea3d1a19cff2d61e3ab
Oliver Vujica <oliver139978@gmail.com>
2024-11-04 16:11:12 +0100

    Dodan UserRepository

---
+++ src/main/java/ba/sum/fpmoz/elearningplatform/repository/UsersRepository.java
@@ -0,0 +1,8 @@
+package ba.sum.fpmoz.elearningplatform.repository;
+
+import ba.sum.fpmoz.elearningplatform.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsersRepository extends JpaRepository<User, Long> {
+
+}
