beca567c234e9a979c14081c9833e0ceb49d7f98
Oliver Vujica <oliver139978@gmail.com>
2024-11-11 16:22:51 +0100

    Register route added

--- src/main/java/ba/sum/fpmoz/elearningplatform/controller/MyFirstController.java
+++ src/main/java/ba/sum/fpmoz/elearningplatform/controller/MyFirstController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -26,4 +27,15 @@ public class MyFirstController {
         model.addAttribute("users", users);
         return "users";
     }
-}
+
+    @GetMapping("/register")
+    public String register() {
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String registerNewUser(User user) {
+            this.usersRepository.save(user);
+            return "redirect:/users";
+        }
+    }
