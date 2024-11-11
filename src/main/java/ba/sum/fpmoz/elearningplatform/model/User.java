5e5b6ebb5f64a31319669b97e68443e07ff88cac
Oliver Vujica <oliver139978@gmail.com>
2024-11-11 16:23:06 +0100

    Added fields for email and pass

--- src/main/java/ba/sum/fpmoz/elearningplatform/model/User.java
+++ src/main/java/ba/sum/fpmoz/elearningplatform/model/User.java
@@ -5,10 +5,12 @@ import jakarta.persistence.*;
 @Entity
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
     private String firstname;
     private String lastname;
+    private String email;
+    private String password;
 
     public User() {
     }
@@ -23,11 +25,35 @@ public class User {
         return id;
     }
 
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
     public String getFirstname() {
         return firstname;
     }
 
     public String getLastname() {
         return lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
